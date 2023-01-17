c9e8af2367b8ee899554ed9445ea491a9329c2e4
thecodingliva <justnur13@gmail.com>
2023-01-17 12:20:21 +0300

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const {ethers} = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  const verifyContract = await ethers.getContractFactory("Verify");
+
+  const deployedVerifyContract = await verifyContract.deploy();
+
+  await deployedVerifyContract.deployed();
+
+  console.log("Verify Contract Address:", deployedVerifyContract.address);
+  console.log("Sleeping......");
+
+  await sleep(40000);
+
+  await hre.run("verify:verify", {
+    address: deployedVerifyContract.address,
+    constructorArguments: [],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
